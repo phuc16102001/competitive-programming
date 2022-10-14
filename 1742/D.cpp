9b004451e8469e3f78be1b6dc4231f606361ca6c
phuc16102001 <phuc16102001@gmail.com>
2022-10-14 22:19:37 +0700

    Fix bug 1742D

--- 1742/D.cpp
+++ 1742/D.cpp
@@ -1,54 +1,52 @@
-    #include <bits/stdc++.h>
-    using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-    #define ll long long
-    #define pi pair<int, int>
-    #define vi vector<int>
-    #define pb push_back
-    #define fi first
-    #define se second
+#define ll long long
+#define pi pair<int, int>
+#define vi vector<int>
+#define pb push_back
+#define fi first
+#define se second
 
-    bool coprime[1001][1001];
+bool coprime[1001][1001];
 
+int gcd(int a, int b) {
+    return (b == 0) ? a : gcd(b, a % b);
+}
 
-    int gcd(int a, int b) {
-        return (b==0)?a:gcd(b,a%b);
+void solve() {
+    int n;
+    int res = -1;
+    cin >> n;
+    vector<int> a(n, 0);
+    vector<int> have(1001, -1);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        have[a[i]] = i;
     }
 
-
-    void solve() {  
-        int n;
-        int res = -1;
-        cin >> n;
-        vector<int> a(n, 0);
-        vector<int> have(1001, false);
-        for (int i=0;i<n;i++) {
-            cin >> a[i];
-            have[a[i]] = i;
-        }
-
-        for (int i=0;i<n;i++){
-            for (int j=1000;j>=1;j--) {
-                if (coprime[a[i]][j] && have[j]) {
-                    res = max(res,i+have[j]+2);
-                }
-            }
-        }
-        cout << res << endl;
-    }
-
-    void preprocess() {
-        for (int i=1;i<=1000;i++) {
-            for (int j=i;j<=1000;j++) {
-                coprime[i][j] = (gcd(i,j)==1); 
-                coprime[j][i] = coprime[i][j];
+    for (int i = 0; i < n; i++) {
+        for (int j = 1000; j >= 1; j--) {
+            if (coprime[a[i]][j] && have[j]!=-1) {
+                res = max(res, i + have[j] + 2);
             }
         }
     }
+    cout << res << endl;
+}
 
-    int main() {
-        preprocess();
-        int nTest;
-        cin >> nTest;
-        while(nTest--) solve();
+void preprocess() {
+    for (int i = 1; i <= 1000; i++) {
+        for (int j = i; j <= 1000; j++) {
+            coprime[i][j] = (gcd(i, j) == 1);
+            coprime[j][i] = coprime[i][j];
+        }
     }
+}
+
+int main() {
+    preprocess();
+    int nTest;
+    cin >> nTest;
+    while (nTest--) solve();
+}
